ca2da1d18153a7df1c5b17b2c19345181e5021aa
Yehor <107763117+yehorkaa@users.noreply.github.com>
2022-11-30 19:51:25 +0200

    new

--- js/array.js
+++ js/array.js
@@ -1,13 +1,18 @@
 let element = prompt('Type the list of commodity you want: ', '');
 const array = [];
+
 array.push(element);
+
 while(element) {
     element = prompt('Type the list of commodity you want: ', '');
     if(element) {
         array.push(element);
     }
+    console.log(array)
 }
 
-array.sort().splice(2,3);
+array.sort()
+console.log(array)
+array.splice(2,3);
 console.log(array);
 
